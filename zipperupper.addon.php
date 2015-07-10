f1935ca1dcd39bf07a96613a3b70df0675de304b
ajkj <thename@requery.me>
2015-07-10 23:54:17 +0900

    ie10 이하에서만

--- zipperupper.addon.php
+++ zipperupper.addon.php
@@ -11,6 +11,9 @@ if(!defined('__XE__')) exit;
 if($called_position !== 'before_display_content') return;
 if(version_compare(PHP_VERSION, '5.3', '<')) return;
 
+if($addon_info->zip_target === 'ie' && preg_match('/MSIE (6|7|8|9|10)/',$_SERVER['HTTP_USER_AGENT']) === 0)
+    return;
+
 include_once 'zipperupper.class.php';
 $zipperupper = new ZipperUpper();
 $zipperupper->zipUp($addon_info->zip_type);
